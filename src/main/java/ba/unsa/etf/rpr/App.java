507b3bea172ec6dfeb34f5cd4f0b6e25555aa8d3
EMIR KALAJDŽIJA <ekalajdzij1@etf.unsa.ba>
2022-11-15 17:00:36 +0100

    Third Commit, zadatk skoro dovršen, testovi dodani, main dodan

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,12 +2,13 @@ package ba.unsa.etf.rpr;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        //evaluateAndPrintResult("( 1 + 2 ) * 3");
+    public static void main( String[] args ) {
+        String input = new String();
+        for (int i = 0; i < args.length; i++)
+                input += args[i];
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
         try {
-            Double d = ExpressionEvaluator.evaluate("( 1 + ( 5 * 20 ) ) ");
-            System.out.println(d);
+            System.out.println(evaluator.evaluate(input));
         } catch (RuntimeException e) {
             System.out.println("Doslo je do izuzetka pogresan unos!");
         }
