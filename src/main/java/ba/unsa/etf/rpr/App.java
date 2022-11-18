63a0c869b594a61cdfd2a8b4c4e63c396985c94f
EMIR KALAJDŽIJA <ekalajdzij1@etf.unsa.ba>
2022-11-18 14:22:57 +0100

    Fourth Commit: - java doc generated - testovi završeni - main završen Zadatak gotov, sve dodano!

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -4,12 +4,11 @@ public class App
 {
     public static void main( String[] args ) {
         String input = new String();
-        String iq = new String();
         for (int i = 0; i < args.length; i++)
                 input += args[i];
         ExpressionEvaluator evaluator = new ExpressionEvaluator();
         try {
-            System.out.println(evaluator.evaluate(("( ( 2 / 1 ) )")));
+            System.out.println(evaluator.evaluate((input)));
         } catch (RuntimeException e) {
             System.out.println("Doslo je do izuzetka pogresan unos!");
         }
--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ExpressionEvaluatorTest {
-    public ExpressionEvaluator testiranje = new ExpressionEvaluator(); // kom
+    public ExpressionEvaluator testiranje = new ExpressionEvaluator();
 
     @Test
     public void testEvaluatorPrvi() {
@@ -14,7 +14,7 @@ public class ExpressionEvaluatorTest {
     }
 
     @Test
-    public void testEvaluatorDrugi() {assertEquals(9, testiranje.evaluate("( 3 * ( sqrt 9 ) )"));}
+    public void testEvaluatorDrugi() {assertEquals(9, testiranje.evaluate("( 3 * ( sqrt ( 9 ) ) )"));}
 
     @Test
     public void testEvaluatorTreci() {assertEquals(101, testiranje.evaluate("( ( 101 ) )"));}
@@ -26,21 +26,26 @@ public class ExpressionEvaluatorTest {
     public void testEvaluatorPeti() {assertEquals(0, testiranje.evaluate("( ( 3 * 3 * 3 ) ) - 27 )"));}
 
     @Test
-    public void testEvaluatorSesti() {assertEquals(3.6502815398728847, testiranje.evaluate("( ( ( sqrt 2 ) + ( sqrt 5 ) ) )"));}
+    public void testEvaluatorSesti() {assertEquals(3.6502815398728847, testiranje.evaluate("( ( ( sqrt ( 2 ) ) + ( sqrt ( 5 ) ) ) )"));}
 
     @Test
-    public void testEvaluatorPosljendji() {assertEquals(0.6502815398728847, testiranje.evaluate("( ( ( sqrt 2 ) + ( sqrt 5 )  ) - 3 )"));}
+    public void testEvaluatorPosljendji() {assertEquals(0.6502815398728847, testiranje.evaluate("( ( ( sqrt ( 2 ) ) + ( sqrt ( 5 ) )  ) - 3 )"));}
+
+
 
     @Test
     public void testIzuzetak() {
-        assertThrows(RuntimeException.class,()->testiranje.evaluate("( (((((188)"));
+        assertThrows(RuntimeException.class,()->testiranje.evaluate(" ( (((((188)"));
         assertThrows(RuntimeException.class,()->testiranje.evaluate("( slovo +  2 ) +/"));
         assertThrows(RuntimeException.class,()->testiranje.evaluate("( -2+  2 )"));
         assertThrows(RuntimeException.class,()->testiranje.evaluate("sqrt256 )"));
         assertThrows(RuntimeException.class,()->testiranje.evaluate("  7+1*3*5/3 "));
         assertThrows(RuntimeException.class,()->testiranje.evaluate("(1+(2*3)/6)"));
         assertThrows(RuntimeException.class,()->testiranje.evaluate("(1 + ( 2 * 3) /6)"));
-        assertThrows(RuntimeException.class,()->testiranje.evaluate("(1 + ( 2 * 3)  2sqrt 2)"));
+        assertThrows(RuntimeException.class,()->testiranje.evaluate("(1 + ( 2 * 3)  2 sqrt 2)"));
+        assertThrows(RuntimeException.class,()->testiranje.evaluate(""));
+        assertThrows(RuntimeException.class,()->testiranje.evaluate("( 3 + sqrt (3) )"));
+        assertThrows(RuntimeException.class,()->testiranje.evaluate(" ( sqrt 15 ) "));
 
 
     }
