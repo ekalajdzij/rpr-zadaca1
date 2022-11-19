d41885c530f865b227d7391da3fea4b96c933a5b
EMIR KALAJDŽIJA <ekalajdzij1@etf.unsa.ba>
2022-11-19 23:42:35 +0100

    Fourth Commit: - java doc generated - testovi završeni - main završen - dodan još jedan dependency Zadatak gotov, sve dodano!

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -3,14 +3,16 @@ package ba.unsa.etf.rpr;
 public class App
 {
     public static void main( String[] args ) {
-        String inputString = new String();
-        for (int i = 0; i < args.length; i++)
-                inputString += args[i];
-        ExpressionEvaluator evaluator = new ExpressionEvaluator();
-        try {
-            System.out.println(evaluator.evaluate((inputString)));
-        } catch (RuntimeException e) {
-            System.out.println("Doslo je do izuzetka pogresan unos!");
+        if (args.length != 0) {
+            String input = new String();
+            for (int i = 0; i < args.length; i++)
+                input += args[i];
+            ExpressionEvaluator evaluator = new ExpressionEvaluator();
+            try {
+                System.out.println(evaluator.evaluate((input)));
+            } catch (RuntimeException e) {
+                System.out.println("Doslo je do izuzetka pogresan unos!");
+            }
         }
     }
     }
